aebed57815dca3169ca6006cf5d35ab5a67c443e
Khailyee <careyyeap@Careys-MacBook-Air.local>
2016-04-28 14:07:15 -0700

    last changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,8 @@
 
 $(document).ready(function () {
 
-	$("#link").click(function () {
+	$("#button").click(function () {
 		$("#about-container").show();
-	});
-
-	$(".textfix").click(function () {
-		$("#about-container").hide();
 	});
 
 	$("#about-overlay").click(function () {
@@ -15,35 +11,31 @@ $(document).ready(function () {
 
   $("#sidebar-button").click(function () {
   if ($(".sidebar-container").hasClass("sidebar-active")) {
-    $("body").removeClass("no-scroll");
     $("#sidebar-button").removeClass("button-active");
     $(".sidebar-container").removeClass("sidebar-active");
-    $(".page-wrapper").removeClass("wrapper-active");
   } else {
     $("#sidebar-button").addClass("button-active");
     $(".sidebar-container").addClass("sidebar-active");
-    $(".page-wrapper").addClass("wrapper-active");
     setTimeout(function() {
       $('body').addClass('no-scroll');
     }, 300);
   }
   });
 
-  $(".background-overlay").click(function () {
+  $(".background-lay").click(function () {
   if ($(".sidebar-container").hasClass("sidebar-active")) {
-    $("body").removeClass("no-scroll");
     $("#sidebar-button").removeClass("button-active");
     $(".sidebar-container").removeClass("sidebar-active");
-    $(".background-overlay").removeClass("wrapper-active");
+    $(".background-lay").removeClass("wrapper-active");
   }
   });
 
   $(".page-wrapper").click(function () {
   if ($(".sidebar-container").hasClass("sidebar-active")) {
-    $("body").removeClass("no-scroll");
     $("#sidebar-button").removeClass("button-active");
     $(".sidebar-container").removeClass("sidebar-active");
-    $(".page-wrapper").removeClass("wrapper-active");
+    $(".london-overlay").removeClass("wrapper-active");
   }
   });
+
 });
